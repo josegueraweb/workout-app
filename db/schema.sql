3808c1008733a59f5c44a8b89cbc89daff38f714
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2023-09-20 20:44:52 -0700

    got rid of the table from the schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,3 @@
 drop database if exists user_db;
+
 create database user_db;
-use user_db;
-create table user(
-    id int not null auto_increment primary key,
-    first_name varchar(50),
-    last_name varchar(50),
-
-
-
-);
