27ef96d86c2fc907b74a9ce575ffd6b4df95ca35
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2023-09-21 20:23:41 -0700

    I added the javascript to handle logging in and logging out

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,5 +1,52 @@
-async function signupHandler(event) {
+async function signupFormHandler(event) {
     event.preventDefault();
-    const username = document.querySelector('#username-signup').value.trim();  
-    const password = document.querySelector('#password-signup').value.trim();
-}
+  
+    const username = document.querySelector('.username-signup').value.trim();  
+    const password = document.querySelector('.password-signup').value.trim();
+    const email = document.querySelector('.email-signup').value.trim();
+  
+    if (username && password && email) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+      if (response.ok) {
+        console.log('Success');
+        alert('New user has been created you are now able to login log in');
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+      }
+    }}
+    async function loginFormHandler(event) {
+      event.preventDefault();
+    
+      const username = document.querySelector('#username-login').value.trim();
+      const password = document.querySelector('#password-login').value.trim();
+    
+      if (username && password) {
+        const response = await fetch('/api/users/login', {
+          method: 'post',
+          body: JSON.stringify({
+            username,
+            password
+          }),
+          headers: { 'Content-Type': 'application/json' }
+        });
+    
+        if (response.ok) {
+          document.location.replace('/');
+        } else {
+          alert(response.statusText);
+        }
+      }
+    }
+    
+    
+document.querySelector('.signup-form').addEventListener('click', signupFormHandler);
+document.querySelector('.btn').addEventListener('click', loginFormHandler);
