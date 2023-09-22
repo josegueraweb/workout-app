0768de9593175eda01dd834f6a7a69f4f85b2cf2
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2023-09-21 19:19:07 -0700

    started working on the login javascript

---
+++ public/javascript/login.js
@@ -0,0 +1,5 @@
+async function signupHandler(event) {
+    event.preventDefault();
+    const username = document.querySelector('#username-signup').value.trim();  
+    const password = document.querySelector('#password-signup').value.trim();
+}
