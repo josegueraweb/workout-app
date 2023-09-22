ab279dd4b0f22a977cdebd6e4438b59c6229eef8
Kathy Hyde <musekat3@gmail.com>
2023-09-21 19:18:25 -0700

    Updates

--- public/script.js
+++ public/script.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3001; // Port number to run the server on
+
+//middleware setup
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 // Display content after login
 const firstname = "[First]"; // Replace with the user's first name
 const welcomeMessage = document.getElementById("welcome-message");
@@ -9,26 +20,4 @@ const userContent = document.createElement("p");
 userContent.textContent = "This is your personalized workout guide.";
 dashboardContent.appendChild(userContent);
 
-// Function to fetch and display exercise data
-function fetchExercise(exerciseNumber) {
-    fetch(`https://api-ninjas.com/api/exercises/${exerciseNumber}`)
-        .then(response => response.json())
-        .then(data => {
-            const exerciseResult = document.getElementById('exercise-result');
-            exerciseResult.innerHTML = `<h2>Exercise ${exerciseNumber}</h2>
-                                        <p>${data.exercise}</p>`;
-        })
-        .catch(error => {
-            console.error('Error fetching exercise:', error);
-        });
-}
 
-// Add click event listeners to the links
-const exerciseLinks = document.querySelectorAll('a[id^="exercise-link-"]');
-exerciseLinks.forEach(link => {
-    link.addEventListener('click', function (event) {
-        event.preventDefault();
-        const exerciseNumber = this.id.split('-')[2]; // Extract the exercise number
-        fetchExercise(exerciseNumber);
-    });
-});
