4acc7addb24b0beed1af936e5736ddc0a3320707
Oseguera, Joshua <joshoseguera09@gmail.com>
2023-09-21 21:35:04 -0700

    roughed in api routes. updated file structure to include view and controller folders. created server.js file and configured it to application needs.

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,27 @@
+
+// Creates an instance of the "Router" object that is available through Express.js
+const router = require('express').Router();
+
+
+// Brings in sequelize models? ... check again later. 
+const { } = require('../models');
+
+
+// Brings it authentication utility? ... check again later. 
+const withAuth = require('../utils');
+
+
+// placeholder for route.
+router.get();
+
+
+// placeholder for route. 
+router.get();
+
+
+// ... placeholder for withAut middleware. 
+router.get();
+
+
+// Exports this file. 
+module.exports = router; 
